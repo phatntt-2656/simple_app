a0a4b8751aab4bf2a54f5b9e89df4a8880749a0e
phatntt1999 <phatntt1999@gmail.com>
2021-08-18 11:59:00 +0700

    Chapter5: Layout with Bootstrap, partials,...

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  scope "(:name)", locale: /en|vi/ do
-    # Ex:- scope :active, -> {where(:active => true)}
-    root "static_pages#home"
-    get "static_pages/home"
-    get "static_pages/help"
-    get "static_pages/about"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope "(:locale)", locale: /en|vi/ do
+    root to: "static_pages#home"
+    get "/home", to: "static_pages#home"
+    get "/help", to: "static_pages#help"
+    get "/about", to: "static_pages#about"
+
+    get "/signup", to: "users#new"
   end
+
 end
